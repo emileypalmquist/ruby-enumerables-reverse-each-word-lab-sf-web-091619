6164bf3666d47953ab04d780016b96abf09d6a96
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-09 02:16:57 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(greeting)
+  sentence = greeting.split(' ')
+  newGreeting = []
+  
+  sentence.each do |word|
+    newGreeting.push(word.reverse)
+  end
+  
+  return newGreeting
+end  
